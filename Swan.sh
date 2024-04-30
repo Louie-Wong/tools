dbdd0423088a4438f3f483524ed1fd8c8c19873c
Josh <Louie@88.com>
2024-04-30 18:22:19 +0800

    Update Swan.sh
    增加几个功能

--- Swan.sh
+++ Swan.sh
@@ -35,21 +35,20 @@ function install_path() {
     echo "Ctrl + C 退出后执行  source ~/.bashrc, 然后再次执行 ./Swan.sh 开始安装节点"
 }
 
-
 function install_node() {
     curl -fsSL https://raw.githubusercontent.com/swanchain/go-computing-provider/releases/ubi/setup.sh | bash
 
-    echo "选择需要安装的版本 1:512MiB 2:32GiB, 输入1或者2"
+    echo "选择需要安装的版本 1->512MiB 2->32GiB, 输入1或者2"
     read -r -p "请确认: " ver_response
 
     case "$ver_response" in
         [1]) 
-            echo "开始下载512MiB版本..."
+            echo "开始下载 512MiB 版本..."
             curl -fsSL https://raw.githubusercontent.com/swanchain/go-computing-provider/releases/ubi/fetch-param-512.sh | bash
             echo "数据下载完成。"
             ;;
         [2]) 
-            echo "开始下载32GiB版本..."
+            echo "开始下载 32GiB 版本..."
             curl -fsSL https://raw.githubusercontent.com/swanchain/go-computing-provider/releases/ubi/fetch-param-32.sh | bash
             echo "数据下载完成。"
             ;;
@@ -65,7 +64,35 @@ function install_node() {
     echo "下载完成"
 }
 
+function init_node() {
+    cd $HOME
 
+    echo "请输入公网IP地址"
+    read -r -p "IP: " host_ip
+    echo "请输入公网端口"
+    read -r -p "Port: " host_port
+     echo "请给节点起个名字 eg. swan001"
+    read -r -p "Node Name: " node_name
+
+    ./computing-provider init --multi-address=/ip4/$host_ip/tcp/$host_port --node-name=$node_name
+
+    echo "初始化完成"
+}
+
+function create_wallet() {
+    cd $HOME
+
+    ./computing-provider wallet new
+
+    echo "创建完成，请妥善保存"
+}
+
+function import_wallet() {
+    cd $HOME
+
+    
+    echo "功能待更新"
+}
 
 # 主菜单
 function main_menu() {
@@ -75,16 +102,19 @@ function main_menu() {
         echo "=======================SWAN节点功能================================"
         echo "退出脚本，请按键盘ctrl c退出即可"
         echo "请选择要执行的操作:"
-        echo "1. 安装节点"
-        echo "2. 创建钱包"
-        echo "3. 导入钱包"
+        echo "1. 配置环境变量"
+        echo "2. 安装节点"
+        echo "3. 初始化节点"
+        echo "4. 创建钱包"
+        echo "5. 导入钱包"
         read -p "请输入选项（1-14）: " OPTION
 
         case $OPTION in
         1) install_path ;;
         2) install_node ;;
-        3) add_wallet ;;
-        4) import_wallet ;;
+        3) init_node ;;
+        4) create_wallet ;;
+        5) import_wallet ;;
         *) echo "无效选项。" ;;
         esac
         echo "按任意键返回主菜单..."
